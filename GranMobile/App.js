4ebd3eb13b2a3905ceb35c4071e15db11d2c3a42
Ribeiro <joao.ribeiro17@outlook.com>
2024-06-20 22:24:13 -0300

    Adicionando o restante de botões para navegação

--- GranMobile/App.js
+++ GranMobile/App.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { FontAwesome6 } from '@expo/vector-icons';
 
 function HomeScreen() {
   return (
@@ -19,15 +21,55 @@ function CoursesScreen() {
   );
 }
 
+function AboutScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Sobre</Text>
+    </View>
+  );
+}
+
+function ContactScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Contato</Text>
+    </View>
+  );
+}
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? <FontAwesome6 name="tachometer-alt-average" size={24} color="black" />
+              : 'ios-information-circle-outline';
+            } else if (route.name === 'Cursos') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            } else if (route.name === 'Sobre') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            } else if (route.name === 'Contatos') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen}  />
         <Tab.Screen name="Cursos" component={CoursesScreen} />
+        <Tab.Screen name="Sobre" component={AboutScreen} />
+        <Tab.Screen name="Contatos" component={ContactScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
