b9e6e93c1bf0bc0f9429c59107d62761a411a052
Ribeiro <joao.ribeiro17@outlook.com>
2024-06-22 15:46:38 -0300

    Adicionando Container de Novidades

--- GranMobile/src/pages/home/index.js
+++ GranMobile/src/pages/home/index.js
@@ -4,7 +4,13 @@ export function Home(){
 
     return(
         <View style={styles.container}>
+          <View style={styles.headerContainer}>
             <Image source={require("../../assets/bannerGran.png")} style={styles.logo} />
+          </View>  
+          <View style={styles.novidadesContainer}>
+            <Text style={{fontSize:20, color:"gray", fontWeight: "bold"}}>Novidades</Text>
+            <Image source={require("../../assets/novidades1.png")} style={styles.noviFoto}/>
+          </View>
         </View>            
     )
 }
@@ -15,10 +21,24 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       backgroundColor: "#f3f3f3",
     },
+    headerContainer:{
+        width: "100%",
+        alignItems: "center"
+    },
     logo:{
-      marginBottom: 60,
+      marginBottom: 30,
       width: '100%',
       height: 180,
       resizeMode: 'contain'
     },
+    novidadesContainer:{
+      alignItems:"center",
+      width: "80%",
+      
+    },
+    noviFoto:{
+      width: 350,
+      resizeMode: 'contain'
+
+    }
 })
